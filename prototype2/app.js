67303a0d9da574818d7b7c1aa56eafd0ba58dee9
richardso21 <richard.so21@sitechhs.com>
2019-12-22 00:31:16 -0500

    button update

--- prototype2/app.js
+++ prototype2/app.js
@@ -55,7 +55,7 @@ function clickfunction(){
 
 //welcome-intro
 // function welcomefunction(){
-//   const welcome = document.querySelectorAll('#welcome-intro path');
+//   const welcome = document.querySelectorAll('.welcome-intro path');
 //   // console.log(welcome);
 //   for(let i = 0; i<welcome.length; i++){
 //     console.log(`Letter ${i} is ${welcome[i].getTotalLength()}`);

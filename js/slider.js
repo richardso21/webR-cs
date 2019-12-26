465ef061b50455a9d2dd44496cde83eeb7cb0d40
richardso21 <richiso899@yahoo.com>
2019-12-25 20:21:45 -0500

    updates

--- js/slider.js
+++ js/slider.js
@@ -2,7 +2,9 @@ window.addEventListener("scroll", function(e) {
   const target = document.querySelector("#slider-content");
 
   var scrolled = window.pageYOffset;
-  var rate = scrolled * -2.75;
+  var rate = scrolled * -2;
 
   target.style.transform = "translate3d(" + rate + "px, 0px, 0px)";
+
+  const textTarget = document.querySelect;
 });
